302cb73ae7a3a816678e75149a62f5272b1e38c4
lanewfu <93032300+lanewfu@users.noreply.github.com>
2021-10-30 17:46:11 +0800

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 16:36:31 2021
+
+@author: USER
+"""
+m = input()
+e = input()
+m = int(m)
+e = int(e)
+if m>90 and e>90:
+    print("有獎品")
+elif m < 60 and e < 60:
+    print("要處罰")
+else:
+    print("再加油")
+    
+    
